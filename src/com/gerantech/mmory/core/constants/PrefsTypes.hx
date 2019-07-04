a04c737ad7fec2d75dbcefd563c7bb98a4296719
Mansour <mansurjavadi@gmail.com>
2019-07-04 20:05:17 +0430

    new challenge tutor-step changed

--- src/com/gerantech/mmory/core/constants/PrefsTypes.hx
+++ src/com/gerantech/mmory/core/constants/PrefsTypes.hx
@@ -45,8 +45,8 @@ class PrefsTypes
 	public static var T_71_SELECT_NAME_FOCUS:Int = 71;
 	public static var T_72_NAME_SELECTED:Int = 72;
 
-	public static var T_73_CHALLENGES_SHOWN:Int = 73;
-	public static var T_74_CHALLENGE_SELECTED:Int = 74;
+	public static var T_201_CHALLENGES_SHOWN:Int = 201;
+	public static var T_202_CHALLENGE_SELECTED:Int = 202;
 
 	public function new() {}
 	public static function isSettings(type:Int) : Bool

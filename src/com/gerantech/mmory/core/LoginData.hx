bc90d991e59bcbd68812115c067deb1b8442b7f8
Mansour <mansurjavadi@gmail.com>
2019-07-14 17:57:21 +0430

    versionup

--- src/com/gerantech/mmory/core/LoginData.hx
+++ src/com/gerantech/mmory/core/LoginData.hx
@@ -11,9 +11,9 @@ import com.gerantech.mmory.core.utils.maps.IntIntMap;
 class LoginData
 {
 	public static var coreSize:Int = 0;
-	public var coreVersion:String = "2100.0711103432";//do not change len.
-	public var noticeVersion:Int = 2000;
-	public var forceVersion:Int = 2000;
+	public var coreVersion:String = "2100.0714141440";//do not change len.
+	public var noticeVersion:Int = 2100;
+	public var forceVersion:Int = 2100;
 	public var resources:IntIntMap;
 	public var buildingsLevel:IntIntMap;
 	public var exchanges:IntList;

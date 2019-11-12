d2dd1e0aee26b821fdfc63b7dbea587d8547bee2
Mansour <mansurjavadi@gmail.com>
2019-11-12 09:45:39 +0330

    removed old tile-map codes fixed wrong targets order

--- src/com/gerantech/mmory/core/battle/fieldes/FieldData.hx
+++ src/com/gerantech/mmory/core/battle/fieldes/FieldData.hx
@@ -12,7 +12,6 @@ class FieldData {
 	public var json:Dynamic;
 	public var times:IntList;
 	public var mapData:String;
-	// public var tileMap:TileMap;
 	public var physics:Colleagues2d;
 	public var targets:Array<Float>;
 
@@ -24,24 +23,21 @@ class FieldData {
 		this.times.push(120);
 		this.times.push(180);
 		this.times.push(240);
-		
+
 		this.targets = new Array();
-		// var version = cast(arg, Int);
-		// trace("version==>" + version);
-		// parse json layout and occupy tile map
-		// this.tileMap = new TileMap();
 		this.physics = new Colleagues2d(BattleField.DELTA_TIME);
 		this.mapData = mapData;
 		this.json = Json.parse(mapData);
 		var children:Array<Dynamic> = this.json.children[0].artboard.children;
 
-		// this.tileMap.setTileState(o.transform.tx, o.transform.ty, o.shape.width, o.shape.height, TileMap.STATE_OCCUPIED);
-		for (o in children)
-			if (o.name == "obstacle")
-				this.physics.add(new Obstacle(o.transform.tx + o.shape.width * 0.5, o.transform.ty + o.shape.height * 0.5, o.shape.width, o.shape.height))
-					.setStatic();
-			else if (o.name == "target")
-				this.targets.concat([o.transform.tx, o.transform.ty]);
+		for (o in children) {
+			if (o.name == "obstacle") {
+				this.physics.add(new Obstacle(o.transform.tx + o.shape.width * 0.5, o.transform.ty + o.shape.height * 0.5, o.shape.width * 0.5, o.shape.height * 0.5));
+			} else if (o.name == "target") {
+				this.targets.unshift(o.transform.ty);
+				this.targets.unshift(o.transform.tx);
+			}
+		}
 	}
 
 	public function isOperation():Bool {

5cb76250895cf64a0bb3ae7b6a176ef67017871c
Mansour <mansurjavadi@gmail.com>
2019-11-14 17:16:27 +0330

    added polygon support

--- src/com/gerantech/colleagues/Shape.hx
+++ src/com/gerantech/colleagues/Shape.hx
@@ -32,12 +32,120 @@ class Shape {
 		return shape;
 	}
 
+	static public function create_poly(path:Array<Dynamic>):Shape {
+		var shape = new Shape(TYPE_POLY, 100);
+		var len = Math.floor(path.length / 3);
+		var _xs = new Array<Float>();
+		var _ys = new Array<Float>();
+		for (i in 0...len) {
+			_xs.push(Std.parseFloat(path[i * 3 + 1]));
+			_ys.push(Std.parseFloat(path[i * 3 + 2]));
+		}
+
+		// Find the right most poon:Int the hull
+		var rightMost = 0;
+		var highestXCoord = _xs[0];
+		var dx:Float = 0;
+		var dy:Float = 0;
+		for (i in 0...len) {
+			var ax = _xs[i];
+			var ay = _ys[i];
+			if (dx < ax)
+				dx = ax;
+			if (dy < ay)
+				dy = ay;
+
+			var x = _xs[i];
+			if (x > highestXCoord) {
+				highestXCoord = x;
+				rightMost = i;
+			}
+			// If matching x then take farthest negative y
+			else if (x == highestXCoord) {
+				if (_ys[i] < _ys[rightMost])
+					rightMost = i;
+			}
+		}
+		// shape.width = dx;
+		// shape.height = dy;
+		shape.radius = Math.sqrt(dx * dx + dy * dy);
+		// trace(radius, dx, dy);
+		var hull = new Array<Int>(); // MAX_POLY_VERTEX_COUNT
+		var outCount:Int = 0;
+		var indexHull:Int = rightMost;
+
+		while (true) {
+			hull[outCount] = indexHull;
+
+			// Search for next index that wraps around the hull
+			// by computing cross products to find the most counter-clockwise
+			// vertex in the set, given the previos hull index
+			var nextHullIndex:Int = 0;
+			for (i in 0...len) {
+				// Skip if same coordinate as we need three unique
+				// points in the set to perform a cross product
+				if (nextHullIndex == indexHull) {
+					nextHullIndex = i;
+					continue;
+				}
+
+				// Cross every set of three unique vertices
+				// Record each counter clockwise third vertex and add
+				// to the output hull
+				// See : http://www.oocities.org/pcgpe/math2d.html
+				// var e1:Vec2 = verts[nextHullIndex].sub(verts[hull[outCount]]);
+				var e1x = _xs[nextHullIndex] - _xs[hull[outCount]];
+				var e1y = _ys[nextHullIndex] - _ys[hull[outCount]];
+				// var e2:Vec2 = verts[i].sub(verts[hull[outCount]]);
+				var e2x = _xs[i] - _xs[hull[outCount]];
+				var e2y = _ys[i] - _ys[hull[outCount]];
+				// var c:Float = Vec2.crossVV(e1, e2);//a.x * b.y - a.y * b.x
+				var c = e1x * e2y - e1y * e2x;
+				if (c < 0.0)
+					nextHullIndex = i;
+
+				// Cross product is zero then e vectors are on same line
+				// therefore want to record vertex farthest along that line
+				if (c == 0.0 && e2x * e2x + e2y * e2y > e1x * e1x + e1y * e1y)
+					nextHullIndex = i;
+			}
+
+			outCount++;
+			indexHull = nextHullIndex;
+
+			// Conclude algorithm upon wrap-around
+			if (nextHullIndex == rightMost) {
+				shape.vertexCount = outCount;
+				break;
+			}
+		}
+
+		// Copy vertices into shape's vertices
+		for (i in 0...shape.vertexCount)
+			shape.set(i, _xs[i], _ys[i]); // vertices[i].set(verts[hull[i]].x, verts[hull[i]].y);
+		
+		// Compute face normals
+		for (i in 0...shape.vertexCount) {
+			// var face:Vec2 = vertices[(i + 1) % vertexCount].sub(vertices[i]);
+			var nx = shape.getX((i + 1) % shape.vertexCount) - shape.getX(i);
+			var ny = shape.getY((i + 1) % shape.vertexCount) - shape.getY(i);
+
+			// Calculate normal with 2D cross product between vector and scalar
+			// normals[i].set(face.y, -face.x);
+			// shape.setNormal(i, nx, -ny);
+			shape.setNormalX(i, CMath.vector_normalizeX(ny, -nx));
+			shape.setNormalY(i, CMath.vector_normalizeY(ny, -nx));
+			// normals[i].normalize();
+		}
+		return shape;
+	}
+
 	public function new(type:Int, radius:Float) {
 		this.type = type;
 		this.radius = radius;
 		this.normals = new Array();
 		this.vertices = new Array();
-		this.matrix = [0.0, 0.0, 0.0, 0.0];
+		this.matrix = [1.0, 0.0, 0.0, 1.0];
 	}
 
 	public function initialize() {

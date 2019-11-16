e80b0d80965b1eef71dd8f82198f6f5e57c33e7e
Mansour <mansurjavadi@gmail.com>
2019-11-16 17:27:50 +0330

    path to polygon for obstacles

--- src/com/gerantech/mmory/core/battle/Obstacle.hx
+++ src/com/gerantech/mmory/core/battle/Obstacle.hx
@@ -4,9 +4,15 @@ import com.gerantech.colleagues.Shape;
 import com.gerantech.colleagues.Colleague;
 
 class Obstacle extends Colleague {
-	public function new(x:Float, y:Float, width:Float, height:Float) {
-		super(-1, null, null, 0, x, y, 0);
-		this.shape = Shape.create_box(width, height);
+	public function new(data:Dynamic) {
+		if (data.shape.type == "rect") {
+			super(-1, null, null, 0, data.transform.tx + data.shape.width * 0.5, data.transform.ty + data.shape.height * 0.5, 0);
+			this.shape = Shape.create_box(data.shape.width * 0.5, data.shape.height * 0.5);
+		} else if (data.shape.type == "path") {
+			super(-1, null, null, 0, data.transform.tx, data.transform.ty, 0);
+			this.shape = Shape.create_poly(cast(data.shape.path, String).split(" "));
+		}
+
 		this.shape.colleague = this;
 		this.shape.initialize();
 		this.setStatic();
